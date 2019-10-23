0f60a798f17fab590c990f2c2032e3a8dab03dd2
Mihael <golob.mihael@gmail.com>
2019-10-23 12:39:48 +0200

    Makes a grid on the screen

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 
 using namespace std;
 
+void drawGrid(Window window, int rows, int columns);
+
 int main(int argc, char *argv[]) {
     Window window;
-    window.InitWindow("SDL", 1000, 600);
+    window.InitWindow("SDL window!", 800, 800);
     window.InitRenderer();
     SDL_Event event;
     bool quit = false;
@@ -24,24 +26,34 @@ int main(int argc, char *argv[]) {
         SDL_RenderClear(window.Renderer);
 
         //draw
-        //renderTexture(texture, ren, 0, 0); //image
 
-        SDL_Rect rect; //rectangle
-        rect.x = 250;
-        rect.y = 150;
-        rect.w = 200;
-        rect.h = 200;
         SDL_SetRenderDrawColor(window.Renderer, 255, 255, 255, 255);
-        SDL_RenderDrawRect(window.Renderer, &rect);
-
-        SDL_RenderDrawLine(window.Renderer, 50, 0, 50, window.height); //line
+        drawGrid(window, 10,10);
 
         //update frame
-        SDL_SetRenderDrawColor(window.Renderer, 0, 0, 0, 255);
+        SDL_SetRenderDrawColor(window.Renderer, 13, 95, 110, 255);
         SDL_RenderPresent(window.Renderer);
     }
 
     window.Close();
 
     return 0;
+}
+
+void drawGrid(Window window, int rows, int columns){
+    int x1 = 0, y1 = 0;
+    int x2 = window.width, y2 = 0;
+    for(int i = 0; i < rows; i++){
+        y1 += window.height/rows;
+        y2 = y1;
+        SDL_RenderDrawLine(window.Renderer, x1, y1, x2, y2);
+    }
+    y1 = 0;
+    y2 = window.height;
+    x1 = 0;
+    for(int i = 0; i < columns; i++){
+        x1 += window.width/columns;
+        x2 = x1;
+        SDL_RenderDrawLine(window.Renderer, x1, y1, x2, y2);
+    }
 }
